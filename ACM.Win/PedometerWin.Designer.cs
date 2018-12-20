9ade75420151ca6110537129bcaf63d804fca590
Johnny Olson <john.olson3@gmail.com>
2018-12-20 12:53:55 -0800

    Add pedometer form with controls

---
+++ ACM.Win/PedometerWin.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ACM.Win
+{
+    partial class PedometerWin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.GoalTextBox = new System.Windows.Forms.TextBox();
+            this.StepsTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ResultLLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CalculateButton.Location = new System.Drawing.Point(605, 68);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(157, 84);
+            this.CalculateButton.TabIndex = 0;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            // 
+            // GoalTextBox
+            // 
+            this.GoalTextBox.Location = new System.Drawing.Point(322, 68);
+            this.GoalTextBox.Name = "GoalTextBox";
+            this.GoalTextBox.Size = new System.Drawing.Size(222, 26);
+            this.GoalTextBox.TabIndex = 1;
+            // 
+            // StepsTextBox
+            // 
+            this.StepsTextBox.Location = new System.Drawing.Point(322, 126);
+            this.StepsTextBox.Name = "StepsTextBox";
+            this.StepsTextBox.Size = new System.Drawing.Size(222, 26);
+            this.StepsTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(73, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Step Goal For Today:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(274, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Number of Steps Today:";
+            // 
+            // ResultLLabel
+            // 
+            this.ResultLLabel.AutoSize = true;
+            this.ResultLLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultLLabel.Location = new System.Drawing.Point(318, 295);
+            this.ResultLLabel.Name = "ResultLLabel";
+            this.ResultLLabel.Size = new System.Drawing.Size(0, 29);
+            this.ResultLLabel.TabIndex = 5;
+            // 
+            // PedometerWin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ResultLLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StepsTextBox);
+            this.Controls.Add(this.GoalTextBox);
+            this.Controls.Add(this.CalculateButton);
+            this.Name = "PedometerWin";
+            this.Text = "PedometerWin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.TextBox GoalTextBox;
+        private System.Windows.Forms.TextBox StepsTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ResultLLabel;
+    }
+}
