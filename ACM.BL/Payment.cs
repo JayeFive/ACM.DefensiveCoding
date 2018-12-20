27e622431e7b8d00383c2b0b888434e0c9c41270
Johnny Olson <john.olson3@gmail.com>
2018-12-20 11:55:04 -0800

    Add process payment method

--- ACM.BL/Payment.cs
+++ ACM.BL/Payment.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace ACM.BL
 {
-    class Payment
+    public class Payment
     {
+        public void ProcessPayment(Payment payment)
+        {
+            // -- Process the payment --
+            // If credit card,
+            // process the credit card payment
+            // If Paypal,
+            // process the Paypal payment
+            // If there is a payment problem, notify the user.
+            // Open a connection
+            // Set stored procedure parameters with the payment data.
+            // Call the save stored procedure
+        }
     }
 }
--- ACM.Win/WinOrder.cs
+++ ACM.Win/WinOrder.cs
@@ -18,17 +18,24 @@ namespace ACM.Win
             var customer = new Customer();
             // Populate the customer instance
 
-            var customerRepository = new CustomerRepository();
-            customerRepository.Add(customer);
-
             var order = new Order();
             // Populate the order instance
+
+            var allowSplitOrders = true;
+
+            var payment = new Payment();
+            // Populate the payment instance
+
+            var customerRepository = new CustomerRepository();
+            customerRepository.Add(customer);
 
             var orderRepository = new OrderRepository();
             orderRepository.Add(order);
 
             var inventoryRepository = new InventoryRepository();
-            inventoryRepository.OrderItems(order);
+            inventoryRepository.OrderItems(order, allowSplitOrders);
+
+            payment.ProcessPayment(payment);
         }
     }
 }
