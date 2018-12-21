f4533063a74cc4d2cce81526542b1c3a4ac36bb9
Johnny Olson <john.olson3@gmail.com>
2018-12-21 11:42:01 -0800

    fix name

---
+++ ACM.Win/OrderWin.cs
@@ -0,0 +1,35 @@
+﻿using ACM.BL;
+using Core.Common;
+using System;
+using System.Windows.Forms;
+
+namespace ACM.Win
+{
+    public partial class OrderWin : Form
+    {
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PlaceOrder();
+        }
+
+        private void PlaceOrder()
+        {
+            var customer = new Customer();
+            // Populate the customer instance
+
+            var order = new Order();
+            // Populate the order instance
+
+            //var allowSplitOrders = true;
+            //var emailReceipt = true;
+
+            var payment = new Payment();
+            // Populate the payment instance
+
+            var orderController = new OrderController();
+
+            orderController.PlaceOrder(customer, order, payment, allowSplitOrders:false, emailReceipt:true);
+        }
+
+    }
+}
